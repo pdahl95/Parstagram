efba638a84aca85d2d173d60f9df00c27f2b82ae
Pernille Dahl <pdahl@csumb.edu>
2020-03-05 19:31:53 -0800

    added comment and username to UI

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -36,7 +36,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @objc func updateTableView(){
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.user"])
         query.limit = 20
         
         query.findObjectsInBackground {(posts, error) in
@@ -58,9 +58,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         let post = posts[section]
-        let comments = post["comments"] as? [PFObject]
-        
-        return 1
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1
     }
     
     
@@ -69,24 +68,35 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let post = posts[indexPath.row]
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+                    
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            cell.captionLabel.text = post["caption"] as? String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
         
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
-        cell.captionLabel.text = post["caption"] as? String
-        
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        
-//        print(url)
-        
-        cell.photoView.af_setImage(withURL: url)
-        
-
-        return cell
+            cell.photoView.af_setImage(withURL: url)
+            
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["user"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
+    
     }
     
     
